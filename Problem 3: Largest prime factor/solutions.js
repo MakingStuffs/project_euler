653a38098d3a662ea92f96f388787919f434f992
Making Stuffs <33229318+MakingStuffs@users.noreply.github.com>
2020-01-07 14:11:26 +0000

    Improved efficiency with a while loop

--- Problem 3: Largest prime factor/solutions.js
+++ Problem 3: Largest prime factor/solutions.js
@@ -1,23 +1,21 @@
 function largestPrimeFactor(number) {
-    if(isPrime(number)) return number;
-    return primeFactors(number);
-  }
-  
-  function isPrime(num) {
-    if(num <= 3) return true;
-    for( let i = 2; i < num; i++) {
-      if(num % i === 0) return false;
+    // Set the starting prime
+    let prime = 2,
+        rtn = 1;
+    // Initiate a loop which will only run while
+    // prime is less than number
+    while (prime <= number) {
+        // Check that the current prime is a factor of the current number
+        number % prime === 0 ?
+            // if it is set the return value to the current prime
+            (rtn = prime,
+                // Then decrement the initial number so we can start from there
+                // on the next run
+                // If it isn't factor increment the current prime
+                number /= prime) : prime++;
     }
-    return true;
-  }
-  
-  function primeFactors(num) {
-    let primeFact;
-    for(let i = 2; i < num; i++) {
-      if(isPrime(i) && num % i === 0) primeFact = i;
-    }
-    return primeFact;
-  }
-  
-  largestPrimeFactor(13195);
-  
+    // Return the final value.
+    return rtn;
+}
+
+largestPrimeFactor(13195);
